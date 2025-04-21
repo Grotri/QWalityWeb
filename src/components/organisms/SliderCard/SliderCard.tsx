07f7f50bf2dda4546c3ae9062b40d417fc8fdd70
Darya Sushkova <dasha.030105@gmail.com>
2025-04-21 13:15:53 +0300

    feat: add slider card organism component
    QW-62

---
+++ src/components/organisms/SliderCard/SliderCard.tsx
@@ -0,0 +1,41 @@
+import { FC } from "react";
+import { ISliderCard } from "./types";
+import styles from "./SliderCard.module.scss";
+import Button from "../../atoms/Button";
+import Radio from "../../atoms/Radio";
+import clsx from "clsx";
+
+const SliderCard: FC<ISliderCard> = ({
+  id,
+  currentId,
+  title,
+  description,
+  radioLabels,
+  price,
+  onPress,
+}) => (
+  <div className={styles.card}>
+    <div className={styles.mainInfo}>
+      <span className={styles.title}>{title}</span>
+      <div className={styles.line} />
+      <span className={styles.description}>{description}</span>
+      <div className={styles.radios}>
+        {radioLabels.map((label: string, index: number) => (
+          <Radio label={label} isChecked key={index} />
+        ))}
+      </div>
+    </div>
+    <div className={styles.subInfo}>
+      <span className={styles.price}>{price} руб. в месяц</span>
+      {currentId !== undefined && currentId === id ? (
+        <span className={clsx(styles.btn, styles.choosenBtn)}>Выбрано</span>
+      ) : (
+        <Button color="welcomeBrightBlue" style={styles.btn} onPress={onPress}>
+          Выбрать
+        </Button>
+      )}
+    </div>
+  </div>
+);
+
+export default SliderCard;

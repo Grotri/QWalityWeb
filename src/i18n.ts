97d1a852f168737d750cf9fb835de0e57edff487
Darya Sushkova <dasha.030105@gmail.com>
2025-05-26 14:10:13 +0300

    fix: add correct defects translations for three languages
    QW-159

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,6 +2,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import translationRU from "./locales/ru/translation.json";
 import translationENG from "./locales/eng/translation.json";
+import translationFR from "./locales/fr/translation.json";
 
 i18n.use(initReactI18next).init({
   fallbackLng: "ru",
@@ -9,6 +10,7 @@ i18n.use(initReactI18next).init({
   resources: {
     ru: { translation: translationRU },
     eng: { translation: translationENG },
+    fr: { translation: translationFR },
   },
   interpolation: {
     escapeValue: false,
